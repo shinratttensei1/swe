acff0853458ec37c1a8a0e0aa8498cc896fa07ff
csoenjoyer <yessenzhan.aitbay@nu.edu.kz>
2024-11-20 03:47:07 +0500

    css is settled up

--- bazaar/app/index.tsx
+++ bazaar/app/index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Miras ne vagina.</Text>
+      <Text className="text-3xl font-pblack">Bazaar!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}>FreakBob profile</Link>
     </View>
